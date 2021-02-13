51f95b711198189c1112e1b13f68186869615ba0
Jeren <jerenshamuhamedova@gmail.com>
2021-02-12 18:51:03 -0600

    Our first Scenario ;))

--- src/test/java/com/cybertek/pages/Ebay_Home.java
+++ src/test/java/com/cybertek/pages/Ebay_Home.java
@@ -1,4 +1,16 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Ebay_Home {
+    public Ebay_Home(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//input[@id='gh-ac']")
+    public WebElement searchButton;
+
 }
--- src/test/java/com/cybertek/step_definitions/Ebay_Step_Definitions.java
+++ src/test/java/com/cybertek/step_definitions/Ebay_Step_Definitions.java
@@ -1,4 +1,36 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.Ebay_Home;
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+
 public class Ebay_Step_Definitions {
+
+    @Given("User is on the page")
+    public void user_is_on_the_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Electronics"));
+
+    }
+    @When("User sends {string} to search bar")
+    public void user_sends_to_search_bar(String string) {
+
+        Ebay_Home ebay_home = new Ebay_Home();
+        ebay_home.searchButton.sendKeys(string+ Keys.ENTER);
+    }
+    @Then("Title should contain {string}")
+    public void title_should_contain(String string) {
+
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedInTitle = string;
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
+
 }
